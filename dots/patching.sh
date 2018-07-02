710a0ccb7a49db8eb7e0709eb6d808d12cd76bbd
Piotr Gorski <lucjan.lucjanov@gmail.com>
2018-07-02 21:23:32 +0200

    Nothing special
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- dots/patching.sh
+++ dots/patching.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 srcdir=some-path
 cd $srcdir
-for p in ../000{1,2,3,4,5,6,7,8,9}-*.patch; do
+# for p in ../000{1,2,3,4,5,6,7,8,9}-*.patch; do
+for p in ../000*-*.patch; do
 echo "---> patching source with $p <---"
 patch -Np1 -i "$p"
-done 
+done
