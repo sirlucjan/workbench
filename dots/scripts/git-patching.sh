8dc7e57e22ee894d42585f570ec1c56761a19e9c
Piotr Gorski <lucjan.lucjanov@gmail.com>
2019-11-27 01:15:31 +0100

    Smoothing
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- dots/scripts/git-patching.sh
+++ dots/scripts/git-patching.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 srcdir=some-path
 cd $srcdir
-# for p in ../000{1,2,3,4,5,6,7,8,9}-*.patch; do
-for p in ../000*-*.patch; do
+for p in ../0*-*.patch; do
 echo "---> patching source with $p <---"
 git apply --whitespace=fix "$p"
 done
